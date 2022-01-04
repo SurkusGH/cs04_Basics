765812809c0c2e1246c2fab258c14d4ce369a904
Surkus <v.surkus@me.com>
2022-01-04 14:51:01 +0200

    Initial commit

---
+++ cs04_paskaita/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace cs04_paskaita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cs04_PASKAITA!");
+
+            #region TEORIJA -
+
+            // 
+            // 
+            // 
+
+            #endregion
+
+
+        }
+
+        public static void Assignment1()
+        {
+
+        }
+
+        public static void Assignment2()
+        {
+
+        }
+
+        public static void Assignment3()
+        {
+
+        }
+
+        public static void Assignment4()
+        {
+
+        }
+
+        public static void Assignment5()
+        {
+
+        }
+
+        public static void Assignment6()
+        {
+
+        }
+
+        public static void Assignment7()
+        {
+
+        }
+
+        public static void Assignment8()
+        {
+
+        }
+
+    }
+}
+
